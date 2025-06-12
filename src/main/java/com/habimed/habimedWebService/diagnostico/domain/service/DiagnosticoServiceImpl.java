8195af8fa429222d4d0049f5136406d4a205da93
RafaelSolier <rafael.solier@utec.edu.pe>
2025-06-12 13:40:31 -0500

    Cambios en controller y service de Diagnostico

--- src/main/java/com/habimed/habimedWebService/diagnostico/domain/service/DiagnosticoServiceImpl.java
+++ src/main/java/com/habimed/habimedWebService/diagnostico/domain/service/DiagnosticoServiceImpl.java
@@ -20,34 +20,41 @@ public class DiagnosticoServiceImpl implements DiagnosticoService {
     private final DiagnosticoRepository diagnosticoRepository;
     private final ModelMapper modelMapper;
 
+    @Override
     public List<DiagnosticoDTO> getAllDiagnosticosWithDetails(DiagnosticoRequest request) {
         return diagnosticoRepository.getAllDiagnosticos(request);
     }
 
+    @Override
     public DiagnosticoDTO getDiagnosticoByIdWithDetails(Integer idDiagnostico) {
         return diagnosticoRepository.getDiagnosticoById(idDiagnostico);
     }
 
+    @Override
     public DiagnosticoResponseDto getDiagnosticoByIdDiagnostico(Integer idDiagnostico){
         Diagnostico diagnostico = diagnosticoRepository.getById(idDiagnostico);
         return modelMapper.map(diagnostico, DiagnosticoResponseDto.class);
     };
 
+    @Override
     public List<DiagnosticoResponseDto> getDiagnosticoByIdCita(Integer idCita){
         List<Diagnostico> diagnosticos = diagnosticoRepository.getByIdCita(idCita);
         return diagnosticos.stream()
                 .map(x -> modelMapper.map(x, DiagnosticoResponseDto.class)).collect(Collectors.toList());
     };
 
+    @Override
     public DiagnosticoResponseDto addDiagnostico(DiagnosticoRequest request) {
         Integer id =  diagnosticoRepository.setDiagnostico(request);
         return this.getDiagnosticoByIdDiagnostico(id);
     }
 
+    @Override
     public DiagnosticoResponseDto updateDiagnostico(Integer idDiagnostico, DiagnosticoRequest request){
         return modelMapper(diagnosticoRepository.update(idDiagnostico, request) , DiagnosticoResponseDto.class);
     }
 
+    @Override
     public Boolean deleteDiagnostico(Integer iddiagnostico) {
         return diagnosticoRepository.deleteDiagnostico(iddiagnostico);
     }
